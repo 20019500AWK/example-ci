cb454175a4c92e37234addaf0ace2ea2366e5a88
20019500AWK <20019500@tafe.wa.edu.au>
2024-02-29 15:14:17 +0800

    Step 5

--- test_functions.py
+++ test_functions.py
@@ -6,9 +6,8 @@ def test_add():
     assert add(2, 3) == 5
     assert add('space', 'ship') == 'spaceship'
 
-#umcomment this in step 5
-#def test_subtract():
-#   assert subtract(2, 3) == -1
+def test_subtract():
+   assert subtract(2, 3) == -1
     
 # uncomment this in step 11
 #def test_convert_farenheit_to_celcius():
