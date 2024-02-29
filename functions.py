5ab2e494d8111b8ec6650112c97dcd8f47516d12
20019500AWK <20019500@tafe.wa.edu.au>
2024-02-29 14:46:40 +0800

    step 1 of the task

---
+++ functions.py
@@ -0,0 +1,11 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a + b #fix this later
+
+def multiply(a, b):
+    return a * b
+
+def convert_fahrenheiht_to_celsius(fahrenheit):
+    return multiply(subtract(fahrenheit, 32), 9 / 5) #fix this later
